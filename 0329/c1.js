9a05621c76ee7ad4fdb04daa0bcddee613231e2a
Martin-L-Bruno <martin.l.bruno-1@ou.edu>
2024-03-29 10:21:54 -0500

    1

--- 0329/c1.js
+++ 0329/c1.js
@@ -1,39 +1,48 @@
-'use strict';
-let R = null;
-let b1 = document.querySelector("#b1");
-let chart1 = null;//for chart to be shown in c1 canvas
-async function loadd()
-{
-    let url = `https://juxinglong.github.io/static/data/states.json`;
 
+'use strict'
+
+let R = null;
+
+let b2 = document.querySelector("#b2");
+
+async function showc() {
+    let url = `https://juxinglong.github.io/static/HW/hw6/MIS3033.json`;
+    //fetch async
     let r = await fetch(url);
     let rj = await r.json();
 
-    let c1 = document.querySelector("#c1");
-    if (chart1 != null) {
-        chart1.destroy();//if already have a chart, delete it 
-    }
-    c1.innerHTML = ``; //clean canvas to show a chart
+    let chart2 = null;
+
+    let c2 = document.querySelector("#c2");
 
     let opts = {
-        type: "pie",
-        
-        data: {
-            labels: rj.map(x => x.st),
-            datasets: [
-                {data:rj.map(x=>x.p),},
-            ],
-        },
-
+        type: "pie", //line or bar can be used too
+        data:
+        {
+            labels: rj.map(x => x.lg),
+            datasets: [{ data: rj.map(x => x.n), label: "number" },],
+        }
     }
 
-    chart1 = new Chart(c1, opts);
+
+
+    if (chart2 != null) {
+        chart2.destroy();
+    }
+    c2.innerHTML = ``;
+
+    chart2 = new Chart(c2, opts);
+    opts = {
+        targets: [c2,],
+        rotate: {value:60, duration:3000},
+
+    }
+    anime(opts);
+};
 
     R = rj;
     console.log(rj);
 
-    Swal.fire("Load data");
-}
 
+    b2.addEventListener("click", showc);
 
-b1.addEventListener("click", loadd);
